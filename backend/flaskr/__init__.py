dbd77d89a76d2e4cc758658149e7f49059b24e1b
josephhensh <josephhensh2000@gmail.com>
2022-07-06 12:46:06 +0100

    Modify quizzes route: update while loop

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -38,6 +38,12 @@ def create_app(test_config=None):
         ) 
         return response
 
+
+    """
+    @TODO:
+    Create an endpoint to handle GET requests
+    for all available categories.
+    """  
    
     @app.route('/categories')
     def get_categories():
@@ -53,9 +59,21 @@ def create_app(test_config=None):
             'success': True,
             'categories': all_categories,
             'total': len(categories) 
-        })  
-   
+        }) 
 
+
+    """
+    @TODO:
+    Create an endpoint to handle GET requests for questions,
+    including pagination (every 10 questions).
+    This endpoint should return a list of questions,
+    number of total questions, current category, categories.
+    TEST: At this point, when you start the application
+    you should see questions and categories generated,
+    ten questions per page and pagination at the bottom of the screen for three pages.
+    Clicking on the page numbers should update the questions.
+    """    
+   
     @app.route('/questions')
     def get_questions():       
         try:
@@ -83,6 +101,13 @@ def create_app(test_config=None):
             abort(400)       
 
 
+    """
+    @TODO:
+    Create an endpoint to DELETE question using a question ID.
+    TEST: When you click the trash icon next to a question, the question will be removed.
+    This removal will persist in the database and when you refresh the page.
+    """
+
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
         try:
@@ -100,8 +125,18 @@ def create_app(test_config=None):
 
         except:
             print(sys.exc_info())
-            abort(422)   
+            abort(422) 
 
+
+    """
+    @TODO:
+    Create an endpoint to POST a new question,
+    which will require the question and answer text,
+    category, and difficulty score.
+    TEST: When you submit a question on the "Add" tab,
+    the form will clear and the question will appear at the end of the last page
+    of the questions list in the "List" tab.
+    """        
     
     @app.route('/questions', methods=['POST'])
     def create_question():
@@ -130,8 +165,19 @@ def create_app(test_config=None):
             })
         except:            
             print(sys.exc_info())
-            abort(422)    
+            abort(422)  
 
+
+
+    """
+    @TODO:
+    Create a POST endpoint to get questions based on a search term.
+    It should return any questions for whom the search term
+    is a substring of the question.
+    TEST: Search by any phrase. The questions list will update to include
+    only question that include that string within their question.
+    Try using the word "title" to start.
+    """
    
     @app.route('/questions/search', methods=['POST'])
     def search_questions():
@@ -155,10 +201,18 @@ def create_app(test_config=None):
             })
         else:            
             print(sys.exc_info())
-            abort(404)        
+            abort(404) 
 
 
-   
+
+    """
+    @TODO:
+    Create a GET endpoint to get questions based on category.
+    TEST: In the "List" tab / main screen, clicking on one of the
+    categories in the left column will cause only questions of that
+    category to be shown.
+    """
+
     @app.route('/categories/<int:category_id>/questions')
     def get_category_questions(category_id):
         category = Category.query.get(category_id)
@@ -179,8 +233,19 @@ def create_app(test_config=None):
             })       
         except:            
             print(sys.exc_info())
-            abort(404)     
+            abort(404)  
 
+
+    """
+    @TODO:
+    Create a POST endpoint to get questions to play the quiz.
+    This endpoint should take category and previous question parameters
+    and return a random questions within the given category,
+    if provided, and that is not one of the previous questions.
+    TEST: In the "Play" tab, after a user selects "All" or a category,
+    one question at a time is displayed, the user is allowed to answer
+    and shown whether they were correct or not.
+    """           
    
     @app.route('/quizzes', methods=['POST'])
     def play_quiz():
@@ -197,25 +262,44 @@ def create_app(test_config=None):
         try:
             if quiz_category['id'] == 0:
                 question_list = [question.format() for question in questions]
+                
                 new_quiz_question = random.choice(question_list) 
+              
                 
             else:
                 for question in questions:
                     if question.category == category.id:
                         new_question_list.append(question)
+                print(new_question_list)    
                 question_list = [question.format() for question in new_question_list] 
             
             new_quiz_question = random.choice(question_list)
-            while new_quiz_question['id'] not in previous_questions:
-                return jsonify({
-                    'question': new_quiz_question,
-                    'success': True                
-                })
-        except:
+            while True:
+                if len(previous_questions) == len(question_list):
+                    return jsonify({
+                        'question': None,
+                        'success': True
+                    })
+                    
+                new_quiz_question = random.choice(question_list)
+                if new_quiz_question['id'] not in previous_questions:
+                    return jsonify({
+                        'question': new_quiz_question,
+                        'success': True
+                    })
+                else:
+                    continue                                                
+                
+        except:            
             print(sys.exc_info())
             abort(404)
 
 
+    """
+    @TODO:
+    Create error handlers for all expected errors
+    including 404 and 422.
+    """      
     ### Error Handlers
     @app.errorhandler(400)
     def bad_request(error):
